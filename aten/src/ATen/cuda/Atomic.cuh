a14aea9a69fa56476eb0e24d41b54073a7e46d64
Jack Taylor <jack.taylor@amd.com>
2023-03-30 13:18:45 +0000

    Temp2

--- aten/src/ATen/cuda/Atomic.cuh
+++ aten/src/ATen/cuda/Atomic.cuh
@@ -336,7 +336,7 @@ static inline __device__ void gpuAtomicAddNoReturn(at::BFloat16 *address, at::BF
 static inline __device__ void gpuAtomicAddNoReturn(double *address, double val) { gpuAtomicAdd(address, val); }
 
 /* Special case fp32 atomic. */
-#if defined(USE_ROCM)
+#if defined(USE_ROCM) and defined(__gfx908__)
 static inline __device__ void gpuAtomicAddNoReturn(float *address, float val) { atomicAddNoRet(address, val); }
 #else
 static inline __device__ void gpuAtomicAddNoReturn(float *address, float val) { gpuAtomicAdd(address, val); }
