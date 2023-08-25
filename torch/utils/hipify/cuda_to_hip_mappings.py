92546e8b69668bca487286f4009b98c64cd4020f
jataylo <jack.taylor@amd.com>
2023-08-25 00:01:20 +0000

    test

--- torch/utils/hipify/cuda_to_hip_mappings.py
+++ torch/utils/hipify/cuda_to_hip_mappings.py
@@ -2915,6 +2915,7 @@ CUDA_IDENTIFIER_MAP = collections.OrderedDict(
         ("cuEventElapsedTime", ("hipEventElapsedTime", CONV_EVENT, API_DRIVER)),
         ("cuEventQuery", ("hipEventQuery", CONV_EVENT, API_DRIVER)),
         ("cuEventRecord", ("hipEventRecord", CONV_EVENT, API_DRIVER)),
+        ("cuFuncSetAttribute", ("hipFuncSetAttribute", CONV_EVENT, API_DRIVER)),
         ("cuEventSynchronize", ("hipEventSynchronize", CONV_EVENT, API_DRIVER)),
         (
             "cuFuncGetAttribute",
@@ -4968,6 +4969,10 @@ CUDA_IDENTIFIER_MAP = collections.OrderedDict(
             "cudaFuncAttributeMaxDynamicSharedMemorySize",
             ("hipFuncAttributeMaxDynamicSharedMemorySize", CONV_TYPE, API_RUNTIME),
         ),
+        (
+            "CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES",
+            ("hipFuncAttributeMaxDynamicSharedMemorySize", CONV_TYPE, API_RUNTIME),
+        ),        
         (
             "cudaFuncAttributePreferredSharedMemoryCarveout",
             ("hipFuncAttributePreferredSharedMemoryCarveout", CONV_TYPE, API_RUNTIME),
